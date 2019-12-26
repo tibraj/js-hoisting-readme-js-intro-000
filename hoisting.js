42ca8974d4240892ec8abb5ae281e47166645a27
Taulant Ibraj <tibraj94@gmail.com>
2019-12-26 21:28:22 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,27 +1,29 @@
-var callMe = function callMe() {
-
+function callMe(){
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
-callMe();
-var crazy = function() {
-var thisIsCrazy = function() {
+
+function crazy(){
+  //fix the code in here:
+  var thisIsCrazy = function (){
     console.log("hey!!!")
   }
   thisIsCrazy();
 }
-crazy()
-var sayMyName = function() {
-var name;
-  name = "Cricky";
- function sayMy() {
-name = "Kristin";
+
+function sayMyName() {
+  // fix the code in here:
+  var name = "Cricky";
+
+  sayMy();
+
+  function sayMy() {
+    var name = "Kristin";
+
     console.log(name);
   }
-  sayMy();
 }
-sayMyName()()
